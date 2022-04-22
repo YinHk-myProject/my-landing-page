851e15cc42a2f139ad00085e1ddc4488b5c32266
YinHk <>
2022-04-22 11:06:09 -0400

    feat: change setup proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -29,8 +29,4 @@ module.exports = function(app) {
       changeOrigin: true,
     })
   );
-  app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
 };
