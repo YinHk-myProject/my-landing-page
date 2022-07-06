e8b0e2d63aa3fb2dd594535a1db4758d46821462
YinHk <>
2022-07-06 12:02:27 -0400

    feat: add changes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -67,7 +67,7 @@ const Experience = () => {
                 <div className={classes.cardTitle}>
                  <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#1152AD" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
                  <Typography gutterBottom variant="h5" component="h5" sx={{marginTop: 2, marginLeft: 5}}>
-                    Junior Programmer
+                    Programmer
                  </Typography>
                 </div>
             }
